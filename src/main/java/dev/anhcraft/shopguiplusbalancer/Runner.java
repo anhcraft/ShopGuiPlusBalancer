929d9dd49c73043997b23849786f6c227f1d86fc
anhcraft <huynhduyanh123123@gmail.com>
2023-05-12 22:55:58 +0700

    Initial commit

---
+++ src/main/java/dev/anhcraft/shopguiplusbalancer/Runner.java
@@ -0,0 +1,2 @@
+package dev.anhcraft.shopguiplusbalancer;public class Runner {
+}
---
+++ src/main/java/dev/anhcraft/shopguiplusbalancer/ShopGuiPlusBalancer.java
@@ -0,0 +1,17 @@
+package dev.anhcraft.shopguiplusbalancer;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class ShopGuiPlusBalancer extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
